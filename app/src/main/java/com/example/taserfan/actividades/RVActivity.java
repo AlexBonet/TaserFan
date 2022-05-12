86cd2640309abf8ede6346325b4ea0d620450266
AlexBHome <alebonmon@alu.edu.gva.es>
2022-05-12 10:52:24 +0200

    creado actividades y vistas

---
+++ app/src/main/java/com/example/taserfan/actividades/RVActivity.java
@@ -0,0 +1,16 @@
+package com.example.taserfan.actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taserfan.R;
+
+public class RVActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyclerview_activity);
+    }
+}
---
+++ app/src/main/java/com/example/taserfan/actividades/prefe/VistaExtendiad.java
@@ -0,0 +1,16 @@
+package com.example.taserfan.actividades.prefe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taserfan.R;
+
+public class VistaExtendiad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extended_view);
+    }
+}
